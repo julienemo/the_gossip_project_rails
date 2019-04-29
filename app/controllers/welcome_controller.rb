3cfb2b55506b9a5544321bfcc248e329b73cb4bc
forkhembo <forkhembo@riseup.net>
2019-04-29 15:53:01 +0200

    save this fucking commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,11 @@ class WelcomeController < ApplicationController
    
   end
   def index_post
-    #puts "hello depuis le serv"
-    #user = User.new
-    #user.first_name = params["first_name"]
-    #user.save
-    #puts params
-    #puts "c'est fini"
+    puts "hello depuis le serv"
+    user = User.new
+    user.first_name = params["first_name"]
+    user.save
+    puts "c'est fini"
     render template: "welcome/index"
     @user = User.find_by(first_name: params[:first_name])
   end
