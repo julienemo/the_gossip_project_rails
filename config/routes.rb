fd0506aca336e4e0d89ae711c343339519e943d0
forkhembo <forkhembo@riseup.net>
2019-04-29 19:11:02 +0200

    update some alignments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get root "home#show"
   get "/pages/:page" => "pages#show"
-  #get "/welcome", to: "welcome#index"
   post "/welcome/", to: "welcome#index_post"
-  #get '/welcome/:first_name', to: 'welcome#index_post'
+  get "/welcome/", to: "welcome#index_post"
   get '/gossip/:id', to: 'gossip#each'
 end
