d328aac6b2cb704a52292c671165aa3545347f8f
forkhembo <forkhembo@riseup.net>
2019-04-29 15:45:31 +0200

    welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
   def index 
-    @user = User.first
-    puts "Mon user est #{@user}"
+   
+  end
+  def index_post
+    #puts "hello depuis le serv"
+    #user = User.new
+    #user.first_name = params["first_name"]
+    #user.save
+    #puts params
+    #puts "c'est fini"
+    render template: "welcome/index"
+    @user = User.find_by(first_name: params[:first_name])
   end
 end
